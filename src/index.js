16daaf041ed3e46816b6d4b97fcbc2054ac77f71
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2022-08-15 13:50:30 +0700

    chore: basic setup server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+const server = http.createServer(app);
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
