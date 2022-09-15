4e7f2f6aa1febabdf6a1cc23a9b8b90c1b06a8c8
kienhi123 <kien123@gmail.com>
2022-09-15 10:50:07 +0700

    feat: controllers-category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,12 +1,15 @@
 import Category from '../models/category'
+// eslint-disable-next-line import/order
+import slugify from 'slugify'
 // eslint-disable-next-line import/prefer-default-export
 export const create = async (request, response) => {
+    request.body.slug = slugify(request.body.name)
     try {
         const category = await new Category(request.body).save()
         response.json(category)
     } catch (error) {
-        response(400).json({
-            message: "Không thêm được danh mục"
+        response.status(400).json({
+            message: error.message('Không thêm được danh mục')
         })
     }
 }
@@ -15,21 +18,22 @@ export const list = async (request, response) => {
         const category = await Category.find()
         response.json(category);
     } catch (error) {
-        response(400).json({
-            message: "Không tìm thấy danh mục"
+        response.status(400).json({
+            message: error.message('Không tìm thấy danh mục')
         })
     }
 }
 export const read = async (request, response) => {
     const condition = { _id: request.params.id }
+    request.body.slug = slugify(request.body.name)
     try {
         const category = await Category.findOne(condition).exec();
         response.json({
             category,
         })
     } catch (error) {
-        response(400).json({
-            message: "Không tìm được danh mục"
+        response.status(400).json({
+            message: error.message('Không tìm thấy danh mục')
         })
     }
 }
@@ -39,21 +43,22 @@ export const remove = async (request, response) => {
         const category = await Category.findOneAndDelete(condition).exec();
         response.json(category)
     } catch (error) {
-        response(400).json({
-            message: "Xóa danh mục thất bại"
+        response.status(400).json({
+            message: error.message('Xóa danh mục thất bại')
         })
     }
 }
 export const update = async (request, response) => {
-    const condition = { _id: request.params.id };
+    const condition = { slug: request.params.slug };
+    request.body.slug = slugify(request.body.name)
     const document = request.body;
     const options = { new: true }
     try {
         const category = await Category.findOneAndUpdate(condition, document, options).exec();
         response.json(category)
     } catch (error) {
-        response(400).json({
-            message: "Cập nhật danh mục thất bại"
+        response.status(400).json({
+            message: error.message("Cập nhật danh mục thất bại")
         })
     }
 }
