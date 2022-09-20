4fceb212242f62693412d6da56c027d4d89c9c05
truongph17848 <truongndph17848@fpt.edu.vn>
2022-09-20 10:11:58 +0700

    feat: update model-service

--- src/models/order.js
+++ src/models/order.js
@@ -1,33 +1,31 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from 'mongoose';
 
 const { ObjectId } = mongoose.Types;
 
-const serviceSchema = new Schema({
+const serviceSchema = new Schema(
+  {
     dateStart: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     infoUser: {
-        type: String,
-        required: true
-
+      type: String,
+      required: true,
     },
     status: {
-        type: Number,
-        required: true
-
+      type: Number,
+      required: true,
     },
     user_id: {
-        type: ObjectId,
-        ref: "User",
-
+      type: ObjectId,
+      ref: 'User',
     },
     service_id: {
-        type: ObjectId,
-        ref: "Service",
+      type: ObjectId,
+      ref: 'Service',
     },
+  },
+  { timestamps: true }
+);
 
-
-}, { timestamps: true });
-
-export default mongoose.model("Order", serviceSchema);
+export default mongoose.model('Order', serviceSchema);
