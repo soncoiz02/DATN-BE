04e412330c5387727a64b83c70211bece8679da0
Minhkb <minhkhongbavi@gmail.com>
2022-09-22 16:55:38 +0700

    feat: apiUser

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,12 +6,51 @@ export const GetoneUser = async (request, response) => {
   try {
     const user = await User.findOne(condition).exec();
     response.json({
-      user,
+      username: user.username,
+      email: user.email,
     });
   } catch (error) {
     console.log(error);
     response.status(400).json({
       message: error.message,
     });
+  }
+};
+// eslint-disable-next-line consistent-return
+export const listUser = async (req, response) => {
+  try {
+    const user = await User.find().sort({ createAt: -1 }).exec();
+    response.json(user);
+    console.log(user);
+  } catch (error) {
+    return response.status(400).json({
+      message: error.message,
+    });
+  }
+};
+// eslint-disable-next-line import/prefer-default-export
+export const removeUser = async (request, response) => {
+  const filter = { _id: request.params.id };
+  try {
+    const user = await User.findOneAndDelete(filter);
+    response.json({
+      message: 'Đã xóa thành công',
+      data: user,
+    });
+  } catch (error) {
+    response.status(400).json({ message: 'Không thể xóa' });
+  }
+};
+export const updateUser = async (request, response) => {
+  const options = { new: true };
+  try {
+    const user = await User.findOneAndUpdate(
+      { id: request.params.id },
+      request.body,
+      options
+    ).exec();
+    response.json(user);
+  } catch (error) {
+    response.status(400).json({ message: error.message });
   }
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,17 @@
 import { Router } from 'express';
-import { GetoneUser } from '../controllers/user';
+import {
+  GetoneUser,
+  listUser,
+  removeUser,
+  updateUser,
+} from '../controllers/user';
 
 const router = Router();
 
 router.get('/user/:id', GetoneUser);
+router.get('/user', listUser);
+router.put('/user/:id', updateUser);
+
+router.delete('/user/:id', removeUser);
 
 export default router;
