8cb127cfec95ddb884c4718e15c4e942f470005b
truongph17848 <truongndph17848@fpt.edu.vn>
2022-09-29 10:17:30 +0700

    feat: sort by rated-avg

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import Service from '../models/service';
 import ServiceStep from '../models/serviceStep';
 import ServiceRating from '../models/serviceRating';
@@ -93,6 +94,45 @@ export const read = async (req, res) => {
         avg: ratedAvg,
       },
     });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+export const sort = async (req, res) => {
+  try {
+    const service = await Service.find({}).exec();
+    let rated = await ServiceRating.find({}).exec();
+    const steps = await ServiceStep.find({}).exec();
+    const newService = service.map((item) => {
+      const serviceRated = rated.filter((rate) =>
+        rate.serviceId.equals(item._id)
+      );
+      const serviceStep = steps.filter((step) =>
+        step.serviceId.equals(item._id)
+      );
+      return {
+        // eslint-disable-next-line no-underscore-dangle
+        ...item._doc,
+        steps: serviceStep,
+        rated: {
+          total: serviceRated.length,
+          avg: (
+            serviceRated.reduce((prev, rateItem) => prev + rateItem.rate, 0) /
+            serviceRated.length
+          ).toFixed(2),
+        },
+      };
+    });
+    if (req.query.order === '1') {
+      // rated: avg from high to low
+      res.json(newService.sort((a, b) => b.rated.avg - a.rated.avg));
+    } else {
+      // rated: avg from low to high
+      res.json(newService.sort((a, b) => a.rated.avg - b.rated.avg));
+    }
   } catch (error) {
     res.status(400).json({
       message: error.message,
--- src/routes/service.js
+++ src/routes/service.js
@@ -1,11 +1,22 @@
 import { Router } from 'express';
-import { create, list, read, remove, update } from '../controllers/service';
+import {
+  create,
+  list,
+  read,
+  remove,
+  sort,
+  update,
+} from '../controllers/service';
 
 const route = Router();
 
 route.post('/service', create);
 // list
 route.get('/service', list);
+
+// sort by rated theo avg        /api/service/sortByRated?order=0-1     0 đánh giá từ nhỏ-> lớn  1 ngược lại nhé
+route.get('/service/sortByRated', sort);
+
 // remove
 route.delete('/service/:id', remove);
 // update
