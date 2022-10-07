9ef3f66334642044e63e50d6d5bf5057e840f284
Minhkb <minhkhongbavi@gmail.com>
2022-10-07 14:54:46 +0700

    fix: sortByRated Store

--- src/controllers/advancedStore.js
+++ src/controllers/advancedStore.js
@@ -1,4 +1,5 @@
 import Store from '../models/store';
+import StoreRating from '../models/storeRating';
 
 // eslint-disable-next-line import/prefer-default-export
 export const searchStore = async (request, response) => {
@@ -22,15 +23,34 @@ export const listStoreByName = async (request, response) => {
     response.status(400).json({ message: error.message });
   }
 };
-export const filterByRate = async (request, response) => {
+
+export const sortByRated = async (request, response) => {
   try {
-    const store = await Store.find({
-      rateId: request.query.rateId.split(','),
-    }).populate('rateId', 'rate content');
-    console.log(store);
-    response.json(store);
+    const store = await Store.find({}).exec();
+    const rated = await StoreRating.find({}).exec();
+    const newStore = store.map((item) => {
+      const storeRated = rated.filter((rate) =>
+        // eslint-disable-next-line no-underscore-dangle
+        rate.storeId.equals(item._id)
+      );
+      return {
+        // eslint-disable-next-line no-underscore-dangle
+        ...item._doc,
+        rated: {
+          total: storeRated.length,
+          avg: (
+            storeRated.reduce((prev, rateItem) => prev + rateItem.rate, 0) /
+            storeRated.length
+          ).toFixed(2),
+        },
+      };
+    });
+    if (request.query.order === '2') {
+      response.json(newStore.sort((a, b) => b.rated.avg - a.rated.avg));
+    } else {
+      response.json(newStore.sort((a, b) => a.rated.avg - b.rated.avg));
+    }
   } catch (error) {
-    console.log(error);
     response.status(400).json({ message: error.message });
   }
 };
