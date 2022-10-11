975a0c389e895d6bdbd819f6eb556006dcf8dc4c
Minhkb <minhkhongbavi@gmail.com>
2022-10-11 23:12:35 +0700

    feat: filterStorebyRated

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import {
+  filterByRate,
   listStoreByName,
   searchByAddress,
   searchStore,
@@ -18,12 +19,12 @@ const router = Router();
 
 router.post('/store', createStore);
 router.get('/store', listStore);
-router.get('/store/:id', storeDetail);
-router.delete('/store/:id', deleteStore);
-router.put('/store/:id', updateStore);
 router.get('/searchName', searchStore);
 router.get('/searchAddress', searchByAddress);
 router.get('/store/storeName', listStoreByName);
-router.get('/store/sortByRated', sortByRated);
-
+router.get('/sortByRated', sortByRated);
+router.get('/filterByRated', filterByRate);
+router.get('/store/:id', storeDetail);
+router.delete('/store/:id', deleteStore);
+router.put('/store/:id', updateStore);
 export default router;
