673d1ba900bf528b76bf9c199c8cd3b1c0579e3e
Bảo Sơn Trần <sontb@toprate.io>
2022-11-01 16:24:51 +0700

    feat: update user api

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,12 +6,13 @@ import {
   removeUser,
   updateUser,
 } from '../controllers/user';
+import { verifyToken } from '../middlewares/token';
 
 const router = Router();
 
 router.get('/user/:id', GetoneUser);
 router.get('/user', listUser);
-router.put('/user/:id', updateUser);
+router.put('/user', verifyToken, updateUser);
 
 router.delete('/user/:id', removeUser);
 router.get('/store-staff/:id', getStoreStaff);
