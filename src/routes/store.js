f8fb3fa3618cb2a8d07d990b3a3cf33699bfd2f5
truongph17848 <truongndph17848@fpt.edu.vn>
2022-11-02 17:34:05 +0700

    feat: revenue store

--- src/routes/store.js
+++ src/routes/store.js
@@ -12,7 +12,7 @@ import {
   deleteStore,
   listStore,
   storeDetail,
-  storeRevenue,
+  stRevenue,
   updateStore,
 } from '../controllers/store';
 
@@ -28,5 +28,5 @@ router.get('/filterByRated', filterByRate);
 router.get('/store/:id', storeDetail);
 router.delete('/store/:id', deleteStore);
 router.put('/store/:id', updateStore);
-router.get('/store/:id/Revenue', storeRevenue);
+router.get('/store-revenue/:id', stRevenue);
 export default router;
