334171a6b73aa96449b91bca343090e926507307
soncoiz02 <soncoiz3107@gmail.com>
2022-12-31 16:30:35 +0700

    feat: fix stuffs

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
       '703579968493-uticdlqnqmbm6p2q8lg4r4f86987qlec.apps.googleusercontent.com',
     clientSecret: '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',
     refreshToken:
-      '1//04ZDA-dXrNmpmCgYIARAAGAQSNwF-L9IrKyeGpL1pbCVtLYmWTVnEPcGk00ahk3JluHSHZnxnf9Q3J17QTqxt3iJJ6-iQ6s-31jM',
+      '1//04fmJfs3g8XvvCgYIARAAGAQSNwF-L9IrMZXJEGhnH7nF-0DZYze6zsmuCOwz6jsKHAQMCHiXNwLZ6NUU4A8g7mBisF0YWJEDJj4',
   },
 });
 
